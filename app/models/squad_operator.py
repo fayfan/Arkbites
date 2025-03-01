6694a660ba58e2b91a4cdb9c1084b82198568ed4
fayfan <sbaltz98@gmail.com>
2025-03-01 17:59:22 -0600

    Troubleshooting production deploy

--- app/models/squad_operator.py
+++ app/models/squad_operator.py
@@ -6,18 +6,20 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 squad_operators = db.Table(
     "squad_operators",
     db.Model.metadata,
-    db.Column("id", db.Integer, primary_key=True),
+    # db.Column("id", db.Integer, primary_key=True),
     db.Column(
         "squad_id",
         db.Integer,
         db.ForeignKey(add_prefix_for_prod("squads.id"), ondelete="CASCADE"),
         nullable=False,
+        primary_key=True,
     ),
     db.Column(
         "operator_id",
         db.Integer,
         db.ForeignKey(add_prefix_for_prod("user_operators.id"), ondelete="CASCADE"),
         nullable=False,
+        primary_key=True,
     ),
     db.Column("created_at", db.DateTime, default=datetime.today),
     db.Column(
