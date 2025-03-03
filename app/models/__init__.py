3a4758a40c0b8e5b5b8eb7f40aa1a94834997148
fayfan <sbaltz98@gmail.com>
2025-03-03 10:43:07 -0600

    Troubleshoot production

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,8 +2,8 @@ from .db import db, environment, SCHEMA
 from .material import Material
 from .operator import Operator
 from .squad import Squad
-from .squad_operator import squad_operators
 from .user import User
 from .user_favorite_operator import user_favorite_operators
 from .user_material import UserMaterial
 from .user_operator import UserOperator
+from .squad_operator import squad_operators
