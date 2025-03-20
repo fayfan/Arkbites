0085b85d91585b6507180c651cbddd5ac0c1be26
fayfan <sbaltz98@gmail.com>
2025-03-20 15:03:03 -0500

    Troubleshooting Render deploy

--- app/__init__.py
+++ app/__init__.py
@@ -47,11 +47,12 @@ CORS(app)
 # any request made over http is redirected to https
 @app.before_request
 def https_redirect():
-    if os.environ.get("FLASK_ENV") == "production":
-        if request.headers.get("X-Forwarded-Proto") == "http":
-            url = request.url.replace("http://", "https://", 1)
-            code = 301
-            return redirect(url, code=code)
+    # Below line commented out while troubleshooting Render deploy (Mixed Content error)
+    # if os.environ.get("FLASK_ENV") == "production":
+    if request.headers.get("X-Forwarded-Proto") == "http":
+        url = request.url.replace("http://", "https://", 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 @app.after_request
